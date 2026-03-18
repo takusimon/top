368a7a792c886bd1cf5679b6f340936654020dba
Taku Nyabadza <67196996+takusimon@users.noreply.github.com>
2026-03-17 19:39:26 -0500

    Thinking more
    instead of just blind syntax learning we are learning to think more

---
+++ js/isogram.js
@@ -0,0 +1,9 @@
+function isIsogram(str){
+   // create variable to store letters of given word
+  // turn letters into lowercase so it works for all words
+  let chars = str.toLowerCase();
+  // split word into its letters
+  chars = chars.split("");
+}
+
+console.log(isIsogram("mOose"));
